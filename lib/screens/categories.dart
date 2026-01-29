c1694c8806c84e1e7ae5866f7b1c16718772009d
Nikola Stankovic <jonnywintech@gmail.com>
2026-01-29 13:53:02 +0100

    setting up model and initial data

---
+++ lib/screens/categories.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pick your category')),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: const [
+          Text(
+            'Category 1',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'Category 2',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'Category 3',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'Category 4',
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
