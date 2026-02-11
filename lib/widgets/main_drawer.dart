527cdae0340012c51a46137b1fd502830ddfc3a5
Nikola Stankovic <jonnywintech@gmail.com>
2026-02-11 08:31:09 +0100

    fixed depricated functions

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -16,7 +16,7 @@ class MainDrawer extends StatelessWidget {
                   Theme.of(context).colorScheme.primaryContainer,
                   Theme.of(
                     context,
-                  ).colorScheme.primaryContainer.withOpacity(0.8),
+                  ).colorScheme.primaryContainer.withValues(alpha: 0.8),
                 ],
               ),
             ),
@@ -44,12 +44,12 @@ class MainDrawer extends StatelessWidget {
             leading: Icon(
               Icons.restaurant,
               size: 26,
-              color: Theme.of(context).colorScheme.onBackground,
+              color: Theme.of(context).colorScheme.onSurface,
             ),
             title: Text(
               'Meals',
               style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
+                color: Theme.of(context).colorScheme.onSurface,
                 fontSize: 24,
               ),
             ),
@@ -59,12 +59,12 @@ class MainDrawer extends StatelessWidget {
             leading: Icon(
               Icons.settings,
               size: 26,
-              color: Theme.of(context).colorScheme.onBackground,
+              color: Theme.of(context).colorScheme.onSurface,
             ),
             title: Text(
               'Filters',
               style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
+                color: Theme.of(context).colorScheme.onSurface,
                 fontSize: 24,
               ),
             ),
